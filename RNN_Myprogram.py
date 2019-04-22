bbd481ad76c2c3ef2b11d0565f8a91b85bd72471
Deepansh Pandey <easypandey.dp@gmail.com>
2019-04-22 11:41:02 +0200

    Project Created
    Project Created

---
+++ RNN_Myprogram.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 10:43:20 2019
+
+@author: tdpco
+"""
+
+# Part 1 - Data Preprocessing
+# Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Reading the dataset
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2]
+
+# Feature Scaling
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 Timesteps and 1 Output
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0],
+                               X_train.shape[1],
+                               1))
+# Part 2 - Building the RNN
+# Initilaizing the RNN
+regressor = Sequential()
+
+# Adding the first LSTM Layer
+regressor.add(LSTM(units=50,
+                   return_sequences=True,
+                   input_shape=(X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding the second LSTM  Layer
+regressor.add(LSTM(units=50,
+                   return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding the Third LSTM  Layer
+regressor.add(LSTM(units=50,
+                   return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding the Fourth LSTM  Layer
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(0.2))
+
+# Adding the output layers
+regressor.add(Dense(units=1))
+
+# Compiling the RNN
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+# Fitting the RNN to training set
+regressor.fit(X_train, y_train, epochs=100, batch_size=32)
+
+# Making Prediction and visualizing Result
+# Getting the real stock price of 2017
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train['Open'],
+                           dataset_test['Open']), axis=0)
+inputs = dataset_total[len(dataset_total)-len(dataset_test)-60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0],
+                    X_test.shape[1], 1))
+# Predicting
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualizing the final Result
+plt.plot(real_stock_price, color='red', label='Real Google Stock Price')
+plt.plot(predicted_stock_price, color='blue',
+         label='Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction by TDP')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
